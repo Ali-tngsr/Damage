74b822cabf568243196f2f652d42be35b592d7e5
Ali <ali.igc1377@gmail.com>
2026-06-13 12:19:26 +0330

    Update build_mesoscale_model.py

--- scripts/build_mesoscale_model.py
+++ scripts/build_mesoscale_model.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Build the stochastic mesoscale Abaqus model for the laminate gauge section."""
 from __future__ import print_function
+
 import os
 import sys
+
 from abaqus import *
 from abaqusConstants import *
 import regionToolset
-import inspect
 
+import inspect
 try:
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 except NameError:
@@ -21,32 +24,44 @@ MODEL_NAME = 'Model-1'
 PART_NAME = 'Specimen'
 PLY90_SET_NAME = 'Ply90_Faces'
 
+
 def _delete_if_exists(repository, name):
     try:
         del repository[name]
     except KeyError:
         pass
 
+
 def _create_engineering_material(model, name, props, orientation):
-    if name in model.materials.keys(): return
+    if name in model.materials.keys():
+        return
     model.Material(name=name)
     nu21 = props['nu12'] * (props['E22'] / props['E11'])
 
     if orientation == '0deg':
-        table = ((props['E11'], props['E22'], props['E22'], props['nu12'], props['nu12'], props['nu23'], props['G12'], props['G12'], props['G23']),)
+        table = ((props['E11'], props['E22'], props['E22'],
+                  props['nu12'], props['nu12'], props['nu23'],
+                  props['G12'], props['G12'], props['G23']),)
     elif orientation == '90deg':
-        table = ((props['E22'], props['E22'], props['E11'], props['nu23'], nu21, nu21, props['G23'], props['G12'], props['G12']),)
+        table = ((props['E22'], props['E22'], props['E11'],
+                  props['nu23'], nu21, nu21,
+                  props['G23'], props['G12'], props['G12']),)
     model.materials[name].Elastic(type=ENGINEERING_CONSTANTS, table=table)
+
 
 def _create_section(model, section_name, material_name):
     if section_name not in model.sections.keys():
         model.HomogeneousSolidSection(name=section_name, material=material_name, thickness=None)
 
+
 def create_model(model_name=MODEL_NAME):
     _delete_if_exists(mdb.models, model_name)
     return mdb.Model(name=model_name)
 
-def build_model(L=70.0, t_0=0.25, t_90=0.5, rho_sat=8.0, seed=42, vf_decimals=1, model_name=MODEL_NAME):
+
+def build_model(L=70.0, t_0=0.25, t_90=0.5, rho_sat=8.0, seed=42,
+                vf_decimals=1, model_name=MODEL_NAME):
+    """Build geometry, partitions, stochastic materials, mesh seeds and sets."""
     model = create_model(model_name)
     t_total = (2.0 * t_0) + t_90
     n_cols = max(1, int(round(rho_sat * L)))
@@ -67,11 +82,31 @@ def build_model(L=70.0, t_0=0.25, t_90=0.5, rho_sat=8.0, seed=42, vf_decimals=1,
     part.PartitionFaceBySketch(faces=part.faces, sketch=horiz)
 
     vert = model.ConstrainedSketch(name='partition_vertical', sheetSize=max(200.0, 2.0 * L))
-    # خطوط عمودی ساده به عنوان مرز مسیرهای ترک (بدون ترفند 1e-4)
     for col_idx in range(1, n_cols):
         x_pos = col_idx * dx
         vert.Line(point1=(x_pos, 0.0), point2=(x_pos, t_total))
     part.PartitionFaceBySketch(faces=part.faces, sketch=vert)
+
+    # =========================================================================
+    # کد جدید: ساخت خودکار ست لبه‌ها برای مسیرهای ترک (Potential_Crack_Edges)
+    # =========================================================================
+    tol = 1.0e-4
+    crack_edges = []
+    for col_idx in range(1, n_cols):
+        x_pos = col_idx * dx
+        # پیدا کردن لبه‌های عمودی که دقیقاً در طول لایه 90 درجه (t_0 تا t_0 + t_90) قرار دارند
+        edges_at_x = part.edges.getByBoundingBox(
+            xMin=x_pos - tol, xMax=x_pos + tol,
+            yMin=t_0 - tol, yMax=t_0 + t_90 + tol,
+            zMin=-tol, zMax=tol
+        )
+        for edge in edges_at_x:
+            crack_edges.append(edge)
+
+    if crack_edges:
+        part.Set(edges=tuple(crack_edges), name='Potential_Crack_Edges')
+        print('SUCCESS: Created Edge Set "Potential_Crack_Edges" with %d lines.' % len(crack_edges))
+    # =========================================================================
 
     vf_field = assign_vf_field(n_cols, 5, seed)
     props_0 = get_properties(45.0, units='MPa')
@@ -101,12 +136,11 @@ def build_model(L=70.0, t_0=0.25, t_90=0.5, rho_sat=8.0, seed=42, vf_decimals=1,
     all_faces = part.faces
     part.MaterialOrientation(
         region=regionToolset.Region(faces=all_faces),
-        orientationType=GLOBAL,
-        axis=AXIS_3,
-        additionalRotationType=ROTATION_NONE,
-        localCsys=None, fieldName='', stackDirection=STACK_3
+        orientationType=GLOBAL, axis=AXIS_3,
+        additionalRotationType=ROTATION_NONE, localCsys=None, fieldName='', stackDirection=STACK_3
     )
     return model, part, vf_field
+
 
 if __name__ == '__main__':
     build_model()
